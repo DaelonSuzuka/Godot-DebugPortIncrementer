ade982b247dd94fd3229792c8d4e060f2be5a681
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-01-16 23:38:39 -0500

    Moved files

--- addons/debug_port_incrementer/plugin.gd
+++ addons/debug_port_incrementer/plugin.gd
@@ -5,14 +5,17 @@ extends Editor
 # ******************************************************************************
 
 
+const default_port = 6007
+const max_port = default_port + 100
+
+
 func _enter_tree():
 	var settings = get_editor_interface().get_editor_settings()
 	var port = settings.get_setting('network/debug/remote_port')
 
-	# default godot debug port is 6007
 	port += 1
-	if port > 6107:
-		port = 6007
+	if port > max_port:
+		port = default_port
 
 	settings.set_setting('network/debug/remote_port', port)
 
